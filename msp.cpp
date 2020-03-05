bae558d00c6950070b8b6233fe59f1397b8675c8
Vasily Golubev <w.golubev@mail.ru>
2020-03-05 23:52:09 +0300

    Add main loop of the program.

--- msp.cpp
+++ msp.cpp
@@ -6,9 +6,23 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+  if (argc != 2) {
+    cout << "Please, specify the name of the config file as: ./msp my_task.cfg" << endl;
+    return 2;
+  }
   cout << "This program calculates wave propagation in acoustic media." << endl;
-  BaseSaver bs1;
-  cout << bs1.getName() << endl;
-  bs1.save();
+  // Config cfg(argv[1]);
+  // Solver solver(cfg);
+  // Saver saver(cfg);
+  unsigned long steps = 101; // Bring from cfg like stoul(cfg.get("steps"));
+  for (unsigned long i = 0; i < steps; i++) {
+    cout << "Step: " << i << endl;
+    //saver.save();
+    //solver->step();
+  }
+  //BaseSaver bs1;
+  //cout << bs1.getName() << endl;
+  //bs1.save();
+  cout << "Program finished." << endl;
   return 0;
 }
