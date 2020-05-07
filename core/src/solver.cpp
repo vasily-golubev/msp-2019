e8377578ea0bf22effe024789daa06867d734b14
ArsenMalkh <arsen.malkhasyan@phystech.edu>
2020-05-07 10:12:16 +0300

    adding (W_1, W, W1)

--- core/src/solver.cpp
+++ core/src/solver.cpp
@@ -14,8 +14,17 @@ void Solver::stepX() {
             rho = 1;
             c = 1;
             AcousticNode& n = this->grid.getAcousticNode(i, j);
-            Vector u = n.getVector();
-            Matrix om = Omega(rho, c).OmegaX();
+	    if(i > 0 && i < grid.getNx() - 1)
+	    {
+            	AcousticNode &n_1 = this->grid.getAcousticNode(i - 1,j);
+		AcousticNode &n1 = this->grid.getAcousticNode(i + 1,j);
+		Vector u = n.getVector();
+                Vector u_1 = n_1.getVector();
+		Vector u1 = n1.getVector();
+		Vector w_1 =u_1 * Omega(rho,c).OmegaX_Inv(); 
+		Vector w =u * Omega(rho,c).OmegaX_Inv();
+		Vector w1 =u1 * Omega(rho,c).OmegaX_Inv();
+            }
         }
     }
 }
@@ -29,8 +38,17 @@ void Solver::stepY() {
             rho = 1;
             c = 1;
             AcousticNode &n = this->grid.getAcousticNode(i, j);
-            Vector u = n.getVector();
-            Matrix om = Omega(rho, c).OmegaY();
+            if(j > 0 && j < grid.getNy() - 1)
+	    {
+            	AcousticNode &n_1 = this->grid.getAcousticNode(i,j - 1);
+		AcousticNode &n1 = this->grid.getAcousticNode(i,j + 1);
+		Vector u = n.getVector();
+                Vector u_1 = n_1.getVector();
+		Vector u1 = n1.getVector();
+		Vector w_1 =u_1 * Omega(rho,c).OmegaY_Inv(); 
+		Vector w =u * Omega(rho,c).OmegaY_Inv();
+		Vector w1 =u1 * Omega(rho,c).OmegaY_Inv();
+            }
         }
     }
 }
